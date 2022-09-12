fa701b916ac6ead82f892c171b95623090d065be
y梦星海s <75596608+Jasonyanyusong@users.noreply.github.com>
2022-09-12 21:59:10 +0800

    Ver 2.1.0 Yusong Yan. Add 1st test dataset (Iris) Current Dataset(s): Iris (From: SKLearn)

---
+++ Application/Iris/Iris.py
@@ -0,0 +1,11 @@
+# IMPORT PACKAGES
+import sklearn
+import numpy as np
+import matplotlib
+from sklearn.datasets import load_iris
+
+# PRINT PACKAGES VERSION
+print(sklearn.__version__)
+print(np.__version__)
+print(matplotlib.__version__)
+
