79ad1a2d968679e3e60cc44f339bd3b1f4267daf
y梦星海s <75596608+Jasonyanyusong@users.noreply.github.com>
2022-09-13 21:51:01 +0800

    Ver 2.5 Yusong Yan.

--- Application/Iris/Iris_Enhanced_V1.py
+++ Application/Iris/Iris_Enhanced_V1.py
@@ -35,7 +35,7 @@ iris_data = dataset_iris['data']
 print(iris_data)
 
 # CHECK IRIS_DATA's DATASTRUCTURE
-for i in range(0,len(dataset_iris['data']),1):
+for i in range(0, len(dataset_iris['data']), 1):
     print("********** Iris Number: ", i, "**********")
     print("Sepal Length (cm): ", iris_data[i][0])
     print("Sepal Width (cm): ", iris_data[i][1])
@@ -118,5 +118,48 @@ Silhouette_Score_Chart.xlabel("Cluster Number:")
 Silhouette_Score_Chart.ylabel("Silhouette Score Score:")
 Silhouette_Score_Chart.plot(range(2, len(silhouette_score_list) + 2), silhouette_score_list)
 Silhouette_Score_Chart.show()
+
+print("DISPLAY TWO KEY POINT ON INERTIA PLOT")
+print("POINT #001: ", "X = ", 1, "Y = ", inertia_list[0])
+print("POINT #150: ", "X = ", 150, "Y = ", inertia_list[149])
+
+print("CALCULATE LINEAR EQUATION OF THE TWO KEY POINT ON INERTIA PLOT")
+linear_k = (inertia_list[len(inertia_list) - 1] - inertia_list[0]) / (len(inertia_list) - 1)
+linear_b = inertia_list[0] - linear_k * 1
+print("LINEAR EQUATION: ", "y = ", linear_k, "x + ", linear_b, "(x != 0)")
+
+print("CALCULATE PLOTS ON THE LINE (AVERAGE CHANGE RATE OF INERTIA)")
+linear_inertia_points_list = []
+plot_real_inertia_list = []
+for i in range(1, len(inertia_list), 1):
+    linear_inertia_point_yVal = i * linear_k + linear_b
+    linear_inertia_points_list.append(linear_inertia_point_yVal)
+    plot_real_inertia_list.append(inertia_list[i - 1])
+    print("Point Number: ", i, " X = ", i, " Y = ", linear_inertia_point_yVal)
+
+print("PLOT ENHANCED INERTIA")
+Enhanced_Inertia_Plot = matplotlib.pyplot
+Enhanced_Inertia_Plot.title("Enhanced Inertia Plot")
+Enhanced_Inertia_Plot.xlabel("Cluster Number:")
+Enhanced_Inertia_Plot.ylabel("Inertia")
+Enhanced_Inertia_Plot.plot(range(1, len(plot_real_inertia_list) + 1), plot_real_inertia_list)
+Enhanced_Inertia_Plot.plot(range(1, len(linear_inertia_points_list) + 1), linear_inertia_points_list)
+Enhanced_Inertia_Plot.show()
+
+print("CALCULATE AND PLOT RELATIVE GAIN COEFFICIENT")
+Relative_Gain_Coefficient_Plot = matplotlib.pyplot
+Relative_Gain_Coefficient_Plot.title("Relative Gain Coefficient Plot")
+Relative_Gain_Coefficient_Plot.xlabel("Cluster Number:")
+Relative_Gain_Coefficient_Plot.ylabel("Relative Gain Coefficient:")
+relative_gain_coefficient_list = []
+for i in range(1, len(linear_inertia_points_list), 1):
+    print("PLIL Length: ", len(plot_real_inertia_list), " Current i: ", i)
+    print("Real Inertia: ", plot_real_inertia_list[i - 1])
+    print("LINEAR AVERAGE INERTIA: ", linear_inertia_points_list[i])
+    Relative_Gain_Coefficient = (linear_inertia_points_list[i] - plot_real_inertia_list[i]) / linear_inertia_points_list[0]
+    print("RELATIVE GAIN COEFFICIENT: ", Relative_Gain_Coefficient)
+    relative_gain_coefficient_list.append(Relative_Gain_Coefficient)
+Relative_Gain_Coefficient_Plot.plot(range(1, len(relative_gain_coefficient_list) + 1), relative_gain_coefficient_list)
+Relative_Gain_Coefficient_Plot.show()
 
 print("CODE ENDS")
