0a5addff6009cab4d8384a45caca4eff1fbb5eda
y梦星海s <75596608+Jasonyanyusong@users.noreply.github.com>
2022-09-13 13:21:14 +0800

    Ver 2.2 Yusong Yan. Add 2nd test dataset (Wine) Current Dataset(s): Iris (From: SKLearn); Wine (From: SKLearn)
    Branch Cluster:
    Merge project files adapted in 9 clusters
    Add: Finished experiment with 5/9 clusters.
    
    NOW TURN TO IRIS DATASET!!!!!!!!
    
    ADDED WINE DATASET

--- Application/Wine/Wine.py
+++ Application/Wine/Wine.py
@@ -1,62 +1,48 @@
-# IMPORT PACKAGES
-import sklearn
-import numpy as np
+import sklearn.datasets as dts
 import matplotlib
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
-import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn import metrics
 
-# PRINT PACKAGES VERSION
-print(sklearn.__version__)
-print(np.__version__)
-print(matplotlib.__version__)
-print(pd.__version__)
+wine_dataset = dts.load_wine()
+wine_dataset_data = wine_dataset.data
+print(len(wine_dataset_data))
+print(wine_dataset_data)
 
-dataset_iris = load_iris()
-print(type(dataset_iris))
-print(dataset_iris.keys())
-
-# TEST DATASET STRUCTURE
-print(dataset_iris)
-print("**********")
-print(dataset_iris['data'])
-# TYPE: - sepal length in cm\n        - sepal width in cm\n        - petal length in cm\n        - petal width in cm\n
-print(len(dataset_iris['data']))
-print(dataset_iris['data'][0])
-print(dataset_iris['data'][len(dataset_iris) - 1])
-print(dataset_iris['data'][0][0])
-
-iris_data = dataset_iris['data']
-print(iris_data)
-
-# CHECK IRIS_DATA's DATASTRUCTURE
-for i in range(0,len(dataset_iris['data']),1):
-    print("********** Iris Number: ", i, "**********")
-    print("Sepal Length (cm): ", iris_data[i][0])
-    print("Sepal Width (cm): ", iris_data[i][1])
-    print("Petal Length (cm): ", iris_data[i][2])
-    print("Petal Width (cm): ", iris_data[i][3])
+for i in range(0, len(wine_dataset_data), 1):
+    print("********** Wine Number: ", i, "**********")
+    print("Value 01: ", wine_dataset_data[i][0])
+    print("Value 02: ", wine_dataset_data[i][1])
+    print("Value 03: ", wine_dataset_data[i][2])
+    print("Value 04: ", wine_dataset_data[i][3])
+    print("Value 05: ", wine_dataset_data[i][4])
+    print("Value 06: ", wine_dataset_data[i][5])
+    print("Value 07: ", wine_dataset_data[i][6])
+    print("Value 08: ", wine_dataset_data[i][7])
+    print("Value 09: ", wine_dataset_data[i][8])
+    print("Value 10: ", wine_dataset_data[i][9])
+    print("Value 11: ", wine_dataset_data[i][10])
+    print("Value 12: ", wine_dataset_data[i][11])
+    print("Value 13: ", wine_dataset_data[i][12])
     print("")
 
 inertia_list = []
 calinsk_harabasz_index_list = []
 davies_bouldin_score_list = []
 silhouette_score_list = []
-for i in range(2, len(iris_data)):
-    iris_i_predicted = KMeans(n_clusters=i)
-    iris_i_predicted.fit_predict(iris_data)
+for i in range(2, len(wine_dataset_data)):
+    wine_i_predicted = KMeans(n_clusters=i)
+    wine_i_predicted.fit_predict(wine_dataset_data)
     print("********** ", i, " CLUSTER(S) **********")
-    print(iris_i_predicted)
-    inertia_list.append(iris_i_predicted.inertia_)
-    print("Inertia: ", iris_i_predicted.inertia_)
-    calinsk_harabasz_index_list.append(metrics.calinski_harabasz_score(iris_data, iris_i_predicted.labels_))
-    print("Calinsk Harabasz Index: ", metrics.calinski_harabasz_score(iris_data, iris_i_predicted.labels_))
-    davies_bouldin_score_list.append(metrics.davies_bouldin_score(iris_data, labels=iris_i_predicted.labels_))
-    print("Davies Bouldin Score: ", metrics.davies_bouldin_score(iris_data, labels=iris_i_predicted.labels_))
-    silhouette_score_list.append(metrics.silhouette_score(iris_data, iris_i_predicted.labels_, metric='euclidean'))
-    print("Silhouette Score: ", metrics.silhouette_score(iris_data, iris_i_predicted.labels_, metric='euclidean'))
+    print(wine_i_predicted)
+    inertia_list.append(wine_i_predicted.inertia_)
+    print("Inertia: ", wine_i_predicted.inertia_)
+    calinsk_harabasz_index_list.append(metrics.calinski_harabasz_score(wine_dataset_data, wine_i_predicted.labels_))
+    print("Calinsk Harabasz Index: ", metrics.calinski_harabasz_score(wine_dataset_data, wine_i_predicted.labels_))
+    davies_bouldin_score_list.append(metrics.davies_bouldin_score(wine_dataset_data, labels=wine_i_predicted.labels_))
+    print("Davies Bouldin Score: ", metrics.davies_bouldin_score(wine_dataset_data, labels=wine_i_predicted.labels_))
+    silhouette_score_list.append(metrics.silhouette_score(wine_dataset_data, wine_i_predicted.labels_, metric='euclidean'))
+    print("Silhouette Score: ", metrics.silhouette_score(wine_dataset_data, wine_i_predicted.labels_, metric='euclidean'))
 print("Inertia: ", inertia_list)
 print("Inertia Length: ", len(inertia_list))
 
--- draft.py
+++ draft.py
@@ -1,2 +1,8 @@
-for i in range (1,10,1):
-    print(i)
+import sklearn.datasets as dts
+dataset_knn = dts.load_boston()
+print(len(dataset_knn))
+print(dataset_knn)
+print(len(dataset_knn['data']))
+print(dataset_knn['data'])
+print(len(dataset_knn['data'][0]))
+print(dataset_knn['data'][0])
